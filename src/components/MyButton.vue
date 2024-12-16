a5c02b0eecb280089265ddb9cfa9a6c087612ab1
weixinyu <2580043641@qq.com>
2024-12-16 15:17:21 +0800

    1

---
+++ src/components/MyButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button :class="`btn ${size}`" @click="handleClick">
+    {{ label }}
+  </button>
+</template>
+
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: '按钮'
+  },
+  size: {
+    type: String,
+    default: 'default'
+  }
+});
+
+
+const emit = defineEmits(['click']);
+
+
+function handleClick() {
+  emit('click');
+}
+</script>
+
+
+<style>
+.btn {
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+}
+.btn.large {
+  font-size: 20px;
+}
+.btn.default {
+  font-size: 16px;
+}
+.btn.small {
+  font-size: 12px;
+}
+</style>
